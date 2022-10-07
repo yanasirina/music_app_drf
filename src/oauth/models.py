71dda5aeb2568b01bcd29ba608a904954dc39848
Yana Sirina <yanasirina0@yandex.ru>
2022-10-08 00:03:57 +0500

    Создала модель пользователей платформы

--- src/oauth/models.py
+++ src/oauth/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+
+class AuthUser(models.Model):
+    """
+    Модель пользователя на платформе
+    """
+    email = models.EmailField(max_length=150, unique=True)
+    join_date = models.DateTimeField(auto_now_add=True)
+    country = models.CharField(max_length=30, blank=True, null=True)
+    city = models.CharField(max_length=30, blank=True, null=True)
+    bio = models.TextField(max_length=2000, blank=True, null=True)
+    display_name = models.CharField(max_length=50, blank=True, null=True)
+    avatar = models.ImageField(
+        upload_to='',
+        blank=True,
+        null=True,
+        validators=[FileExtensionValidator(allowed_extensions=['jpg '])]
+    )
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_authenticated(self):
+        """
+        Всегда возвращает True.
+        Это способ узнать, был ли пользователь аутентифицирован.
+        """
+        return True
